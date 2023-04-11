f04ba4df12ba5324239c333fd9520e1826f8d7b2
Shoaib <Shoaibk909@gmail.com>
2023-04-11 13:43:00 +0530

    searchUI

---
+++ src/container/home/homeLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchBox from './component/searchBar'
+function HomeLayout() {
+  return (
+        <div className='home-bg'>
+            
+    <div className='container'>
+        <SearchBox/>
+        </div>
+    </div>
+  )
+}
+
+export default HomeLayout
