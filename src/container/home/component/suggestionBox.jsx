b0e91c08e1644de07f654a67a532b8bd250e15df
Shoaib <Shoaibk909@gmail.com>
2023-04-11 23:06:25 +0530

    Mobile_ui-done

--- src/container/home/component/suggestionBox.jsx
+++ src/container/home/component/suggestionBox.jsx
@@ -5,6 +5,7 @@ import outfit3 from '../../../Assests/model3.jpg'
 import outfit4 from '../../../Assests/model4.jpg'
 import outfit5 from '../../../Assests/model5.jpg'
 import outfit6 from '../../../Assests/model4.jpg'
+import { Link } from 'react-router-dom'
 const SuggestionBox = () => {
   return (
       <div className='sug-box-container '>
@@ -13,62 +14,64 @@ const SuggestionBox = () => {
     <div className='latest-trends'>
         <h5>Latest Trends</h5>  
     <div className='row'>
-    <div className='col-md-2'>
+    <div className='col-md-2 product-five'>
     <div className='trending-outfit'>
+        <a href="/productpage">
 <img src={outfit1} alt="" width={100} height="100%"/>
+        </a>
     </div>
         <h6 className='text-center'>Trend 1</h6>
     </div>
-    <div className='col-md-2'>
+    <div className='col-md-2 product-five'>
     <div className='trending-outfit'>
+        <a href="/productpage">
 <img src={outfit2} alt="" width={100} height="100%"/>
+        </a>
     </div>
     <h6 className='text-center'>Trend 2</h6>
 
     </div>
-    <div className='col-md-2'>
+    <div className='col-md-2 product-five'>
     <div className='trending-outfit'>
+        <a href="/productpage">
 <img src={outfit3} alt="" width={100} height="100%"/>
+        </a>
     </div>
     <h6 className='text-center'>Trend 3</h6>
 
     </div>
-    <div className='col-md-2'>
+    <div className='col-md-2 product-five'>
     <div className='trending-outfit'>
+        <a href="/productpage">
 <img src={outfit4} alt="" width={100} height="100%"/>
+        </a>
     </div>
     <h6 className='text-center'>Trend 1</h6>
 
     </div>
-    <div className='col-md-2'>
+    <div className='col-md-2 product-five'>
     <div className='trending-outfit'>
+        <a href="/productpage">
 <img src={outfit5} alt="" width={100} height="100%"/>
+        </a>
     </div>
     <h6 className='text-center'>Trend 1</h6>
-
-    </div>
-    <div className='col-md-2'>
-    <div className='trending-outfit'>
-<img src={outfit4} alt="" width={100} height="100%"/>
-    </div>
-    <h6 className='text-center'>Trend 1</h6>
-
     </div>
     </div>
     </div>
     <div className='popular-suggestions'>
         <h5>Popular Suggestions</h5>
     <div className='outfit-title'>
-    <h6>Striped shirt dress</h6>
+    <h6><a href="/productpage" className="link-title"> Striped shirt dress</a></h6>
     </div>
     <div className='outfit-title'>
-    <h6>Striped shirt dress</h6>
+    <h6><a href="/productpage" className="link-title"> Satin shirts</a></h6>
     </div>
     <div className='outfit-title'>
-    <h6>Striped shirt dress</h6>
+    <h6><a href="/productpage" className="link-title"> Denim jumpsuit</a></h6>
     </div>
     <div className='outfit-title'>
-    <h6>Striped shirt dress</h6>
+    <h6><a href="/productpage" className="link-title"> Leather Dresses</a></h6>
     </div>
     </div>
       </div>
