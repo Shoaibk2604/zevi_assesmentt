8bea1559e1f477616658e97363b6c7163e250aed
Shoaib <Shoaibk909@gmail.com>
2023-04-11 20:20:23 +0530

    productpage_done

--- src/container/productpage/components/productCard.jsx
+++ src/container/productpage/components/productCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { AiFillHeart, AiFillStar, AiOutlineHeart } from "react-icons/ai";
-import productimg from "../../../Assests/model3.jpg";
+import productimg from "../../../Assests/model4.jpg";
 const ProductCard = () => {
   const[wishlist,setWishList]= useState(false);
   return (
@@ -12,7 +12,7 @@ const ProductCard = () => {
               <div className="product-image">
                   <img src={productimg} alt="" width={100} />
                   <div className="product-wishlist-btn">
-                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(!wishlist)}/>:<AiOutlineHeart onClick={()=>setWishList(!wishlist)}/>}</span>
+                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(false)}/>:<AiOutlineHeart onClick={()=>setWishList(true)}/>}</span>
                   </div>
                 <div className="product-btn">
                 <button className="btn">View Product</button>
@@ -43,7 +43,7 @@ const ProductCard = () => {
               <div className="product-image">
                   <img src={productimg} alt="" width={100} />
                   <div className="product-wishlist-btn">
-                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(!wishlist)}/>:<AiOutlineHeart onClick={()=>setWishList(!wishlist)}/>}</span>
+                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(false)}/>:<AiOutlineHeart onClick={()=>setWishList(true)}/>}</span>
                   </div>
                 <div className="product-btn">
                 <button className="btn">View Product</button>
@@ -74,7 +74,7 @@ const ProductCard = () => {
               <div className="product-image">
                   <img src={productimg} alt="" width={100} />
                   <div className="product-wishlist-btn">
-                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(!wishlist)}/>:<AiOutlineHeart onClick={()=>setWishList(!wishlist)}/>}</span>
+                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(false)}/>:<AiOutlineHeart onClick={()=>setWishList(true)}/>}</span>
                   </div>
                 <div className="product-btn">
                 <button className="btn">View Product</button>
@@ -105,7 +105,7 @@ const ProductCard = () => {
               <div className="product-image">
                   <img src={productimg} alt="" width={100} />
                   <div className="product-wishlist-btn">
-                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(!wishlist)}/>:<AiOutlineHeart onClick={()=>setWishList(!wishlist)}/>}</span>
+                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(false)}/>:<AiOutlineHeart onClick={()=>setWishList(true)}/>}</span>
                   </div>
                 <div className="product-btn">
                 <button className="btn">View Product</button>
@@ -131,161 +131,7 @@ const ProductCard = () => {
               </div>
             </div>
           </div>
-          <div className="col-md-3">
-            <div className="product-card-wrapper">
-              <div className="product-image">
-                  <img src={productimg} alt="" width={100} />
-                  <div className="product-wishlist-btn">
-                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(!wishlist)}/>:<AiOutlineHeart onClick={()=>setWishList(!wishlist)}/>}</span>
-                  </div>
-                <div className="product-btn">
-                <button className="btn">View Product</button>
-                </div>
-              </div>
-              <div className="product-detail">
-                <div className="product-title">
-                  <h6>Round Neck Cotton Tee</h6>
-                </div>
-                <div className="product-price">
-                  <span className="price">Rs 599</span><span className="offer-price"> Rs 499</span>
-                </div>
-                <div className="product-rating">
-                  <div className="checkClass d-flex align-items-center">
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <span>(150)</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="product-card-wrapper">
-              <div className="product-image">
-                  <img src={productimg} alt="" width={100} />
-                  <div className="product-wishlist-btn">
-                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(!wishlist)}/>:<AiOutlineHeart onClick={()=>setWishList(!wishlist)}/>}</span>
-                  </div>
-                <div className="product-btn">
-                <button className="btn">View Product</button>
-                </div>
-              </div>
-              <div className="product-detail">
-                <div className="product-title">
-                  <h6>Round Neck Cotton Tee</h6>
-                </div>
-                <div className="product-price">
-                  <span className="price">Rs 599</span><span className="offer-price"> Rs 499</span>
-                </div>
-                <div className="product-rating">
-                  <div className="checkClass d-flex align-items-center">
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <span>(150)</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="product-card-wrapper">
-              <div className="product-image">
-                  <img src={productimg} alt="" width={100} />
-                  <div className="product-wishlist-btn">
-                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(!wishlist)}/>:<AiOutlineHeart onClick={()=>setWishList(!wishlist)}/>}</span>
-                  </div>
-                <div className="product-btn">
-                <button className="btn">View Product</button>
-                </div>
-              </div>
-              <div className="product-detail">
-                <div className="product-title">
-                  <h6>Round Neck Cotton Tee</h6>
-                </div>
-                <div className="product-price">
-                  <span className="price">Rs 599</span><span className="offer-price"> Rs 499</span>
-                </div>
-                <div className="product-rating">
-                  <div className="checkClass d-flex align-items-center">
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <span>(150)</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="product-card-wrapper">
-              <div className="product-image">
-                  <img src={productimg} alt="" width={100} />
-                  <div className="product-wishlist-btn">
-                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(!wishlist)}/>:<AiOutlineHeart onClick={()=>setWishList(!wishlist)}/>}</span>
-                  </div>
-                <div className="product-btn">
-                <button className="btn">View Product</button>
-                </div>
-              </div>
-              <div className="product-detail">
-                <div className="product-title">
-                  <h6>Round Neck Cotton Tee</h6>
-                </div>
-                <div className="product-price">
-                  <span className="price">Rs 599</span><span className="offer-price"> Rs 499</span>
-                </div>
-                <div className="product-rating">
-                  <div className="checkClass d-flex align-items-center">
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <span>(150)</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="product-card-wrapper">
-              <div className="product-image">
-                  <img src={productimg} alt="" width={100} />
-                  <div className="product-wishlist-btn">
-                  <span>{wishlist?<AiFillHeart onClick={()=>setWishList(!wishlist)}/>:<AiOutlineHeart onClick={()=>setWishList(!wishlist)}/>}</span>
-                  </div>
-                <div className="product-btn">
-                <button className="btn">View Product</button>
-                </div>
-              </div>
-              <div className="product-detail">
-                <div className="product-title">
-                  <h6>Round Neck Cotton Tee</h6>
-                </div>
-                <div className="product-price">
-                  <span className="price">Rs 599</span><span className="offer-price"> Rs 499</span>
-                </div>
-                <div className="product-rating">
-                  <div className="checkClass d-flex align-items-center">
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <AiFillStar className="checked" />
-                    <span>(150)</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+         
         
         </div>
       </div>
